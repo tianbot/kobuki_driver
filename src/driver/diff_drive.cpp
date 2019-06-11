058d0d148bf099b475227e373ed2f2ba16afe528
Tian Bo <tianb03@hotmail.com>
2019-06-11 20:41:02 +0800

    param modified for edu version

--- src/driver/diff_drive.cpp
+++ src/driver/diff_drive.cpp
@@ -32,9 +32,9 @@ DiffDrive::DiffDrive() :
 //  v(0.0), w(0.0), // command velocities, in [m/s] and [rad/s]
   radius(0.0), speed(0.0), // command velocities, in [mm] and [mm/s]
   point_velocity(2,0.0), // command velocities, in [m/s] and [rad/s]
-  bias(0.345), // wheelbase, wheel_to_wheel, in [m]
-  wheel_radius(0.088), // radius of main wheel, in [m]
-  tick_to_rad(0.005585054f),
+  bias(0.296), // wheelbase, wheel_to_wheel, in [m]
+  wheel_radius(0.0625), // radius of main wheel, in [m]
+  tick_to_rad(0.0095199777f),
   diff_drive_kinematics(bias, wheel_radius)
 {}
 
